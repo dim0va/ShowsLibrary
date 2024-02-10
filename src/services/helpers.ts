dcfa184a5aedf87a88615e001af51527e7d6a732
Yanitsa Dimova <61024966+dim0va@users.noreply.github.com>
2024-02-10 10:26:11 +0200

    Update helpers.ts

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -4,13 +4,11 @@ export const removeHtmlTags = (string: string) =>
   string.replace(/<[^>]*>/g, '');
 
 export const openLink = async (link?: string) => {
-  console.log(link, 'link');
   if (!link) {
     return;
   }
 
   const res = await Linking.canOpenURL(link);
-  console.log('tuka?', res);
 
   if (res) {
     Linking.openURL(link);
